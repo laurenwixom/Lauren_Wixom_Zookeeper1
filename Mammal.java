79f1245bd5a2fcee66d55f5b8ed814aebf7ecb59
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-24 16:30:26 -0500

    Add files via upload

---
+++ Mammal.java
@@ -0,0 +1,13 @@
+package com.codingdojo.zookeeper;
+
+package com.codingdojo.zookeeper;
+
+public class Mammal {
+	Integer energyLevel = 100;
+	
+	public Integer displayEnergy() {
+		System.out.println("Energy level: " + energyLevel);
+		return energyLevel;
+	}
+
+}
